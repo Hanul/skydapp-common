98934c0f6adda8918a8f5b95fc1ae7d1a477b654
Sim Young Jae <hanul@hanul.me>
2022-03-25 21:27:12 +0900

    add waiter

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,5 +1,6 @@
 export { default as EventContainer } from "./EventContainer";
 export { default as Debouncer } from "./Debouncer";
+export { default as Waiter } from "./Waiter";
 export { default as SkyUtil } from "./SkyUtil";
 export { default as SkyDate } from "./SkyDate";
 export { default as SkyLog } from "./SkyLog";
